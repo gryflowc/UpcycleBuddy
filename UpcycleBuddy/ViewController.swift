382935f0b947941127b4352cb6ca476241a83c46
Ella Gryf-Lowczowska <egryflowczowska@googlemail.com>
2019-06-26 13:15:07 +0100

    Initial Commit

---
+++ UpcycleBuddy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UpcycleBuddy
+//
+//  Created by Ella Gryf-Lowczowska on 26/06/2019.
+//  Copyright © 2019 Ella Gryf-Lowczowska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
